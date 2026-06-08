138134661c5a1d23df6cb1afc3dd71e1a0516f68
Ubuntu <ubuntu@ip-172-31-39-51.ap-south-1.compute.internal>
2026-06-08 08:49:20 +0000

    Fixed deploy script image reference

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 IMAGE_NAME=$1
-TAG=$2
 
 docker stop project-3 || true
 docker rm project-3 || true
 
 docker run -d \
---name project-3 \
--p 80:80 \
-${IMAGE_NAME}:${TAG}
+  --name project-3 \
+  -p 80:80 \
+  $IMAGE_NAME
